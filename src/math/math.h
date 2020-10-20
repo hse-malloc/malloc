11ccbb39afdd00472628250df4e877ea91b7a698
Arseny Mitin <mitinarseny@gmail.com>
2020-10-20 11:17:15 +0300

    feat: efficient roundDown and roundUp

--- src/math/math.h
+++ src/math/math.h
@@ -7,23 +7,15 @@ namespace hse::math {
 	// roundDown returns maximum multiple of given multiplier,
 	// which does not exceed given num
 	template<typename T, typename = std::enable_if<std::is_integral<T>::value>>
-	T roundDown(T num, T multiplier) noexcept {
-		if (multiplier == 0)
-			return num;
-		T remainder = num % multiplier;
-		if (remainder == 0)
-			return num;
-		return num - remainder;
+	constexpr T roundDown(T num, T multiplier) noexcept {
+		return num & (-multiplier);
 	}
 
 	// roundDown returns munumum multiple of given multiplier,
 	// which is not less than given num
 	template<typename T, typename = std::enable_if<std::is_integral<T>::value>>
-	T roundUp(T num, T multiplier) noexcept {
-		T down = roundDown<T>(num, multiplier);
-		if (down == num)
-			return num;
-		return down + multiplier;
+	constexpr T roundUp(T num, T multiplier) noexcept {
+		return roundDown<T>(num + multiplier - 1, multiplier);	
 	}
 }
 
