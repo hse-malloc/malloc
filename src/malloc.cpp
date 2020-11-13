08668f5f99c9f8eb0ce8b5b2b43b961137ade993
Arseny Mitin <mitinarseny@gmail.com>
2020-11-13 20:06:52 +0300

    refactor: malloc-implementation -> hse_malloc

--- src/malloc.cpp
+++ src/malloc.cpp
@@ -1,4 +1,4 @@
-#include "malloc/malloc_implementation.h"
+#include "malloc.h"
 #include "math/math.h"
 #include "memory/allocator.h"
 
@@ -6,65 +6,91 @@
 #include <cstdint>
 #include <numeric>
 #include <cerrno>
-#include <new>
+
+#ifdef HSE_MALLOC_DEBUG
+#include <unistd.h>
+#define DEBUG_LOG(msg) ::write(2, msg "\n", sizeof(msg));
+#else
+#define DEBUG_LOG(msg) /* Ignore */
+#endif
+
+namespace hse {
+
+static hse::memory::Allocator _allocator{};
 
 
-namespace std { // NOLINT(cert-dcl58-cpp)
+void *malloc(std::size_t size) noexcept {
+    DEBUG_LOG("MALLOC");
+    if (size == 0) {
+        return nullptr;
+    }
 
-static hse::memory::Allocator a{};
-
-extern "C" {
-
-void *malloc(size_t size) noexcept {
-    return hse::malloc(size);
+    try {
+        // NOLINTNEXTLINT(cppcoreguidelines-pro-type-reinterpret-cast])
+        return reinterpret_cast<void *>(_allocator.alloc(size));
+    } catch (...) {
+        return nullptr;
+    }
 }
 
 void free(void *ptr) noexcept {
-    return hse::free(ptr);
-}
+    DEBUG_LOG("FREE");
+    if (ptr == nullptr) {
+        return;
+    }
 
-void *calloc(size_t count, size_t size) noexcept {
-    return hse::calloc(count, size);
-}
-
-void *realloc(void *ptr, size_t size) noexcept {
-    return hse::realloc(ptr, size);
-}
-
-void *aligned_alloc(size_t alignment, size_t size) noexcept {
-    return hse::aligned_alloc(alignment, size);
-}
-
-} // extern "C"
-} // namespace std
-
-// Should be in global namespace
-// No need to redefine all signatures
-// only for throwing single object declaration
-void* operator new(std::size_t size) {
-    void* ptr = std::malloc(size);
-    if(ptr == nullptr){
-        throw std::bad_alloc{};
+    try {
+        _allocator.free(reinterpret_cast<std::uintptr_t>(ptr));
+    } catch (...) {
     }
 }
 
-void *operator new(std::size_t size, std::align_val_t al) {
-    void* ptr = std::malloc(size);
-    if(ptr == nullptr){
-        throw std::bad_alloc{};
+void *calloc(std::size_t count, std::size_t size) noexcept {
+    DEBUG_LOG("CALLOC");
+    if (count == 0 || size == 0) {
+        return nullptr;
+    }
+
+    try {
+        std::size_t numBytes = count * size;
+        auto *ptr = hse::malloc(numBytes);
+
+        std::iota(reinterpret_cast<std::uint8_t *>(ptr),
+             reinterpret_cast<std::uint8_t *>(ptr) + numBytes,
+             0);
+        return ptr;
+    } catch (...) {
+        return nullptr;
     }
 }
 
-// should be noexcept
-void operator delete(void *ptr) noexcept {
-    std::free(ptr);
+void *realloc(void *ptr, std::size_t size) noexcept {
+    DEBUG_LOG("REALLOC");
+    if (size == 0) {
+        return nullptr;
+    }
+
+    try {
+        return reinterpret_cast<void *>(_allocator.realloc(reinterpret_cast<std::uintptr_t>(ptr), size));
+    } catch (...) {
+        return nullptr;
+    }
 }
 
-void operator delete(void *ptr, std::size_t) noexcept {
-    std::free(ptr);
+void *aligned_alloc(std::size_t alignment, std::size_t size) noexcept {
+    DEBUG_LOG("ALIGNED_ALLOC");
+    if (size == 0 || alignment < sizeof(void*)
+        || !hse::math::isPowerOf2(alignment)
+        || size % alignment != 0) {
+        errno = EINVAL;
+        return nullptr;
+    }
+
+    try {
+        return reinterpret_cast<void *>(_allocator.alloc(size, alignment));
+    } catch (...) {
+        return nullptr;
+    }
 }
 
-void operator delete(void *ptr, std::align_val_t) noexcept {
-    std::free(ptr);
-
-}
+} // namespace hse
