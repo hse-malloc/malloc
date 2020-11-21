35ef10f729723a0b5a715ed27703a7d43d8cbade
Арсений Митин <a.mitin@group-ib.com>
2020-11-21 18:10:16 +0300

    format: lint

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -138,7 +138,7 @@ TEST_CASE("aligned_alloc", "[aligned_alloc][free]") {
     hse::free(ptr);
 }
 
-TEST_CASE("aligned_alloc: invalid aligment", "[aligned_alloc]") {
+TEST_CASE("aligned_alloc: invalid alignment", "[aligned_alloc]") {
     REQUIRE(hse::aligned_alloc(16,  42) == nullptr);
     REQUIRE(hse::aligned_alloc(3,   42) == nullptr);
     REQUIRE(hse::aligned_alloc(67,  42) == nullptr);
@@ -146,7 +146,7 @@ TEST_CASE("aligned_alloc: invalid aligment", "[aligned_alloc]") {
     REQUIRE(hse::aligned_alloc(0,   42) == nullptr);
 }
 
-TEST_CASE("aligned_alloc: big aligment", "[aligned_alloc][free]") {
+TEST_CASE("aligned_alloc: big alignment", "[aligned_alloc][free]") {
     constexpr std::size_t ALIGNMENT = 2UL << 14U;
     constexpr std::size_t SIZE      = ALIGNMENT * 2;
     auto *ptr = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(ALIGNMENT, SIZE * sizeof(std::uint8_t)));
@@ -155,7 +155,7 @@ TEST_CASE("aligned_alloc: big aligment", "[aligned_alloc][free]") {
     hse::free(ptr);
 }
 
-TEST_CASE("aligned_alloc: small aligment", "[aligned_alloc][free]") {
+TEST_CASE("aligned_alloc: small alignment", "[aligned_alloc][free]") {
     constexpr std::size_t ALIGNMENT = 2UL << 6U;
     constexpr std::size_t SIZE      = ALIGNMENT * 20;
     auto *ptr = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(ALIGNMENT, SIZE * sizeof(std::uint8_t)));
@@ -164,92 +164,97 @@ TEST_CASE("aligned_alloc: small aligment", "[aligned_alloc][free]") {
     hse::free(ptr);
 }
 
-TEST_CASE("multiple usage: aligned_alloc with small aligment, "
-                          "less than page malloc, "
-                          "aligned_alloc with big aligment", "[aligned_alloc][malloc][free]") {
+TEST_CASE("aligned_alloc: with small alignment, "
+          "malloc: less than page, "
+          "aligned_alloc: big alignment",
+          "[aligned_alloc][malloc][free]") {
     constexpr std::size_t SMALL_ALIGNMENT = 2UL << 6U;
     constexpr std::size_t SMALL_SIZE      = SMALL_ALIGNMENT * 2;
-    constexpr std::size_t   BIG_ALIGNMENT = 2UL << 14U;
-    constexpr std::size_t   BIG_SIZE      = BIG_ALIGNMENT * 2;
+    constexpr std::size_t BIG_ALIGNMENT   = 2UL << 14U;
+    constexpr std::size_t BIG_SIZE        = BIG_ALIGNMENT * 2;
 
-    auto *ptr_1 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(SMALL_ALIGNMENT, SMALL_SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_1) % SMALL_ALIGNMENT == 0);
-    testArray(std::span{ptr_1, SMALL_SIZE});
+    auto *ptr1 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(SMALL_ALIGNMENT, SMALL_SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr1) % SMALL_ALIGNMENT == 0);
+    testArray(std::span{ptr1, SMALL_SIZE});
 
-    auto *ptr_2 = reinterpret_cast<std::uint8_t *>(hse::malloc(LESS_THAN_PAGE * sizeof(std::uint8_t)));
-    testArray(std::span{ptr_2, LESS_THAN_PAGE});
+    auto *ptr2 = reinterpret_cast<std::uint8_t *>(hse::malloc(LESS_THAN_PAGE * sizeof(std::uint8_t)));
+    testArray(std::span{ptr2, LESS_THAN_PAGE});
 
 
-    auto *ptr_3 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(BIG_ALIGNMENT, BIG_SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_3) % BIG_ALIGNMENT == 0);
-    testArray(std::span{ptr_3, SMALL_SIZE});
+    auto *ptr3 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(BIG_ALIGNMENT, BIG_SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr3) % BIG_ALIGNMENT == 0);
+    testArray(std::span{ptr3, BIG_SIZE});
 
-    hse::free(ptr_1);
-    hse::free(ptr_2);
-    hse::free(ptr_3);
+    hse::free(ptr1);
+    hse::free(ptr2);
+    hse::free(ptr3);
 }
 
-TEST_CASE("multiple usage: aligned_alloc with small aligment + realloc to less than page, "
-                          "aligned_alloc with big aligment", "[aligned_alloc][realloc][free]") {
+TEST_CASE("aligned_alloc: small alignment, "
+          "realloc: to less than page, "
+          "aligned_alloc: big alignment",
+          "[aligned_alloc][realloc][free]") {
     constexpr std::size_t ALIGNMENT = 2UL << 6U;
     constexpr std::size_t SIZE      = ALIGNMENT * 2;
 
-    auto *ptr_1 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(ALIGNMENT, SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_1) % ALIGNMENT == 0);
-    testArray(std::span{ptr_1, SIZE});
+    auto *ptr1 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(ALIGNMENT, SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr1) % ALIGNMENT == 0);
+    testArray(std::span{ptr1, SIZE});
 
-    ptr_1 = reinterpret_cast<std::uint8_t *>(hse::realloc(ptr_1, LESS_THAN_PAGE * sizeof(std::uint8_t)));
-    testArray(std::span{ptr_1, LESS_THAN_PAGE});
+    ptr1 = reinterpret_cast<std::uint8_t *>(hse::realloc(ptr1, LESS_THAN_PAGE * sizeof(std::uint8_t)));
+    testArray(std::span{ptr1, LESS_THAN_PAGE});
 
 
-    auto *ptr_2 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(ALIGNMENT, SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_2) % ALIGNMENT == 0);
-    testArray(std::span{ptr_2, SIZE});
+    auto *ptr2 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(ALIGNMENT, SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr2) % ALIGNMENT == 0);
+    testArray(std::span{ptr2, SIZE});
 
-    hse::free(ptr_1);
-    hse::free(ptr_2);
+    hse::free(ptr1);
+    hse::free(ptr2);
 }
 
-TEST_CASE("multiple usage: aligned_alloc with small aligment, "
-                          "small malloc,"
-                          "realloc 1st ptr to less than page, "
-                          "free 2nd ptr"
-                          "aligned_alloc with medium aligment, "
-                          "big malloc, "
-                          "aligned_alloc with big aligment [aligned_alloc][malloc][realloc][free]") {
-    constexpr std::size_t  SMALL_ALIGNMENT = 2UL << 6U;
-    constexpr std::size_t  SMALL_SIZE      = SMALL_ALIGNMENT * 2;
+TEST_CASE("aligned_alloc: small alignment, "
+          "malloc: small, "
+          "realloc: 1st ptr to less than page, "
+          "free: 2nd ptr, "
+          "aligned_alloc: medium alignment, "
+          "malloc: big, "
+          "aligned_alloc: big alignment",
+          "[aligned_alloc][malloc][realloc][free]") {
+    constexpr std::size_t SMALL_ALIGNMENT  = 2UL << 6U;
+    constexpr std::size_t SMALL_SIZE       = SMALL_ALIGNMENT * 2;
     constexpr std::size_t MEDIUM_ALIGNMENT = 2UL << 10U;
     constexpr std::size_t MEDIUM_SIZE      = MEDIUM_ALIGNMENT * 2;
-    constexpr std::size_t   BIG_ALIGNMENT  = 2UL << 14U;
-    constexpr std::size_t   BIG_SIZE       = BIG_ALIGNMENT * 2;
+    constexpr std::size_t BIG_ALIGNMENT    = 2UL << 14U;
+    constexpr std::size_t BIG_SIZE         = BIG_ALIGNMENT * 2;
 
-    auto *ptr_1 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(SMALL_ALIGNMENT, SMALL_SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_1) % SMALL_ALIGNMENT == 0);
-    testArray(std::span{ptr_1, SMALL_ALIGNMENT});
+    auto *ptr1 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(SMALL_ALIGNMENT, SMALL_SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr1) % SMALL_ALIGNMENT == 0);
+    testArray(std::span{ptr1, SMALL_SIZE});
 
-    auto *ptr_2 = reinterpret_cast<std::uint8_t *>(hse::malloc(SMALL_NUMBER * sizeof(std::uint8_t)));
-    testArray(std::span{ptr_2, SMALL_NUMBER});
+    auto *ptr2 = reinterpret_cast<std::uint8_t *>(hse::malloc(SMALL_NUMBER * sizeof(std::uint8_t)));
+    testArray(std::span{ptr2, SMALL_NUMBER});
 
-    ptr_1 = reinterpret_cast<std::uint8_t *>(hse::realloc(ptr_1, LESS_THAN_PAGE * sizeof(std::uint8_t)));
-    testArray(std::span{ptr_1, LESS_THAN_PAGE});
+    ptr1 = reinterpret_cast<std::uint8_t *>(hse::realloc(ptr1, LESS_THAN_PAGE * sizeof(std::uint8_t)));
+    testArray(std::span{ptr1, LESS_THAN_PAGE});
 
-    hse::free(ptr_2);
+    hse::free(ptr2);
 
-    auto *ptr_3 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(MEDIUM_ALIGNMENT, MEDIUM_SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_3) % MEDIUM_ALIGNMENT == 0);
-    testArray(std::span{ptr_3, MEDIUM_SIZE});
+    auto *ptr3 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(MEDIUM_ALIGNMENT, MEDIUM_SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr3) % MEDIUM_ALIGNMENT == 0);
+    testArray(std::span{ptr3, MEDIUM_SIZE});
 
-    auto *ptr_4 = reinterpret_cast<std::uint8_t *>(hse::malloc(BIG_NUMBER * sizeof(std::uint8_t)));
-    testArray(std::span{ptr_4, BIG_NUMBER});
+    auto *ptr4 = reinterpret_cast<std::uint8_t *>(hse::malloc(BIG_NUMBER * sizeof(std::uint8_t)));
+    testArray(std::span{ptr4, BIG_NUMBER});
 
-    auto *ptr_5 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(BIG_ALIGNMENT, BIG_SIZE * sizeof(std::uint8_t)));
-    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr_5) % BIG_ALIGNMENT == 0);
-    testArray(std::span{ptr_5, BIG_SIZE});
+    auto *ptr5 = reinterpret_cast<std::uint8_t *>(hse::aligned_alloc(BIG_ALIGNMENT, BIG_SIZE * sizeof(std::uint8_t)));
+    REQUIRE(reinterpret_cast<std::uintptr_t>(ptr5) % BIG_ALIGNMENT == 0);
+    testArray(std::span{ptr5, BIG_SIZE});
 
-    hse::free(ptr_1);
+    hse::free(ptr1);
     // already freed ptr_2
-    hse::free(ptr_3);
-    hse::free(ptr_4);
-    hse::free(ptr_5);
+    hse::free(ptr3);
+    hse::free(ptr4);
+    hse::free(ptr5);
 }
+
